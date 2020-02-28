60fa7e8102502be34a837798f44b49ef406c1676
Amir Hajizadeh <amir@tincanvas.com>
2020-02-27 18:02:24 -0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -9,8 +10,9 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
@@ -19,10 +21,24 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters}/>
+      <h1> Monsters Rolodex </h1>
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange={this.handleChange} />
+
+      <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
