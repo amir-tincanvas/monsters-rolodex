76f559705d2852362b5f461ebb851a46c62a2a12
Amir Hajizadeh <amir@tincanvas.com>
2020-02-27 18:01:20 -0800

    Updates

---
+++ precache-manifest.975c6f4520b5825f01fbdc9cca0f4900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e459b96fcec09a0939f8aef3b87a4e14",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80088dcf9803841bd303",
+    "url": "/monsters-rolodex/static/css/main.3741e901.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80088dcf9803841bd303",
+    "url": "/monsters-rolodex/static/js/main.fabad809.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
